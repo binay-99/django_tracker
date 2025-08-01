e74d029ed0c8c7079f0ad8b6b9009aea70c6d683
Binay <binaytilokchandani@gmail.com>
2025-08-01 12:30:58 +0530

    models created

--- first/settings.py
+++ first/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'first.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expense',
+        'USER':'binay',
+        'PASSWORD':'zxcvbnm,./',
+        'HOST':'127.0.0.1',
+        'POST':'5432',
     }
 }
 
